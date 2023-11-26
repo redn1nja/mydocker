a616d73f941db1e4bab1b98098de3f77ce762101
beheni <anastasiia.beheni@ucu.edu.ua>
2023-11-26 15:57:23 +0100

    додала шось сігрупне як замовляли x2

--- include/mycontainer.h
+++ include/mycontainer.h
@@ -14,6 +14,7 @@ public:
     static std::vector<std::string> root_mount_points;
     int pipefd[2];
     // шось неймспейсне софія додасть
+    std::string cgroup_name;
     size_t memory_limit_mb = 5; // actually random values
     size_t pids_limit = 10;
     size_t cpu_proportion = 100;
