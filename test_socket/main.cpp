04d551b4738d69c09e3167846426611b2e836e6b
Sofiia Folvarochna <sofiyafolv@gmail.com>
2023-11-27 12:49:34 +0200

    changed recv and send to readn and writen

--- test_socket/main.cpp
+++ test_socket/main.cpp
@@ -6,29 +6,34 @@
 #include <iostream>
 #include <cstring>
 #include "unistd.h"
+#include "helper_functions.h"
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in server;
     char buf[1024];
     int sd;
-    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         std::cerr << "test socket: failed to create socket" << std::endl;
     }
     memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(8000);
-    int res = bind(sd,(struct sockaddr*) &server, sizeof(server));
-    if (res == -1 ) {
+    int res = bind(sd, (struct sockaddr *) &server, sizeof(server));
+    if (res == -1) {
         std::cerr << "test socket: failed to bind socket" << std::endl;
     }
     listen(sd, 1);
     int psd = accept(sd, nullptr, nullptr);
     close(sd);
     for (;;) {
-        int cc = recv(psd, buf, sizeof(buf), 0);
-        if (cc == 0) exit(EXIT_SUCCESS);
+        int cc = readn(psd, buf, sizeof(buf));
+        if (cc == 0) {
+            writen(psd, "closed", sizeof("closed"));
+            exit(EXIT_SUCCESS);
+        }
         buf[cc] = '\0';
         printf("message received: %s\n", buf);
+        writen(psd, "accepted", sizeof("accepted"));
     }
 }
