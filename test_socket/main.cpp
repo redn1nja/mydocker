9e86a814a9835e960d005c4f1307e5c7874e1173
olesiaomelchuk <olesyao2003@gmail.com>
2023-11-27 12:41:11 +0200

    test server (cpp version)

--- test_socket/main.cpp
+++ test_socket/main.cpp
@@ -1,3 +1,34 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <cstring>
+#include "unistd.h"
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_in server;
+    char buf[1024];
+    int sd;
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        std::cerr << "test socket: failed to create socket" << std::endl;
+    }
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(8000);
+    int res = bind(sd,(struct sockaddr*) &server, sizeof(server));
+    if (res == -1 ) {
+        std::cerr << "test socket: failed to bind socket" << std::endl;
+    }
+    listen(sd, 1);
+    int psd = accept(sd, nullptr, nullptr);
+    close(sd);
+    for (;;) {
+        int cc = recv(psd, buf, sizeof(buf), 0);
+        if (cc == 0) exit(EXIT_SUCCESS);
+        buf[cc] = '\0';
+        printf("message received: %s\n", buf);
+    }
+}
