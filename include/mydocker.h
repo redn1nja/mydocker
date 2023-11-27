5d29b23bd683261a7de72d4c0733dec3c7da93ce
Sofiia Folvarochna <sofiyafolv@gmail.com>
2023-11-27 15:43:57 +0200

    added switch case to execute command from cli

--- include/mydocker.h
+++ include/mydocker.h
@@ -10,6 +10,28 @@
 #include "mycontainer.h"
 #include "signal.h"
 #include <iostream>
+#include <unordered_map>
+
+enum commands {
+    CREATE,
+    RUN,
+    LIST_CONTAINERS,
+    STOP,
+    RESUME,
+    KILL_CONTAINER,
+    LISTEN,
+    DETACH
+};
+
+static std::unordered_map<std::string, commands> commands{{"create",          CREATE},
+                                                          {"run",             RUN},
+                                                          {"list_containers", LIST_CONTAINERS},
+                                                          {"stop",            STOP},
+                                                          {"resume",          RESUME},
+                                                          {"kill_conatiner",  KILL_CONTAINER},
+                                                          {"listen",          LISTEN},
+                                                          {"detach",          DETACH},
+};
 
 class Mydocker {
 private:
@@ -17,20 +39,54 @@ private:
     int attached_container_index = -1;
 public:
     Mydocker() = default;
+
     ~Mydocker() = default;
+
     Mydocker &operator=(const Mydocker &other) = delete;
+
     Mydocker(const Mydocker &other) = delete;
+
     void run(size_t index) { containers[index]->start(); }
+
     template<class... Args>
     void create(Args &&...args) {
         containers.push_back(std::make_unique<Mycontainer>(std::forward<Args>(args)...));
     }
+
     void list_containers();
+
     void stop(size_t index);
+
     void resume(size_t index);
+
     void kill_container(size_t index);
+
     void listen(size_t index);
+
     void detach();
+
+    template<class... Args>
+    void execute_command(const std::string &command, Args ...args) {
+        switch (commands[command]) {
+            case CREATE:
+                create(std::forward<Args>(args)...);
+            case RUN:
+                run(std::forward<Args>(args)...);
+            case LIST_CONTAINERS:
+                list_containers(std::forward<Args>(args)...);
+            case STOP:
+                stop(std::forward<Args>(args)...);
+            case RESUME:
+                resume(std::forward<Args>(args)...);
+            case KILL_CONTAINER:
+                kill_container(std::forward<Args>(args)...);
+            case LISTEN:
+                listen(std::forward<Args>(args)...);
+            case DETACH:
+                detach(std::forward<Args>(args)...);
+        }
+    }
+
 };
 
 #endif //MYDOCKER_MYDOCKER_H
